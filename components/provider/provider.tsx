31aec7db24f5086e6837e10cc4f2161f716d85f2
Karan Chaudhary <tharukaran930@gmail.com>
2023-08-16 22:04:40 +0545

    fixing css

--- components/provider/provider.tsx
+++ components/provider/provider.tsx
@@ -6,7 +6,7 @@ import Navbar from "../navbar/nav-bar";
 function Provider({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = useState(false);
   return (
-    <div className={`font-rem ${!open ? "" : "bg-gray-300"}`}>
+    <div className={`font-rem ${!open ? "" : "bg-gray-300"} flex flex-col gap-12`}>
       <Navbar open={open} setOpen={setOpen} />
       {open ? "" : children}
     </div>
